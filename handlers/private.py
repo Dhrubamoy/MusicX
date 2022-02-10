e2b2e1706c4c6f0dbb4a353c4235ca660415b500
Dhrubamoy <97961618+LEGENDARY-OS@users.noreply.github.com>
2022-02-10 03:52:36 -0800

    Add files via upload

---
+++ handlers/private.py
@@ -0,0 +1,48 @@
+import asyncio
+from time import time
+from datetime import datetime
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/bb7e6f59b3db29b215446.jpg",
+        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [Dhruba'𝘅𝗗 🚬❤️](https://t.me/DhrubaXD)
+
+𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [✨ 𝗠𝗿'Dhruba 🚬 💜](https://t.me/DhrubaXD)
+𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [✨ Group ❤️🎸](https://t.me/Legend_K_Userbot)
+𝐃𝐢𝐬𝐜𝐮𝐬𝐬 :- [✨  Tough 𝗖𝗹𝗮𝗻 🎧](https://t.me/Legend_K_Userbot)
+𝐒𝐨𝐮𝐫𝐜𝐞  :- [✨  𝗖𝗹𝗶𝗰𝗸 ☑️ 𝗥𝗲𝗽𝗼 🌍](https://github.com/LEGENDARY-OS/AIMusicX)
+We are tough:- [✨ 𝗝𝗼𝗶𝗻 ❤️🥀](https://t.me/Legend_K_Userbot)
+
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝗠𝗿'Dhruba 🚬 ❤️](https://t.me/DhrubaXD)**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/Legend_K_Userbot")
+                ]
+                
+           ]
+        ),
+    )
+    
+
+@Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/8c3abf591121615cdef42.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 Cʟɪᴄᴋ Mᴇ Tᴏ Gᴇᴛ Rᴇᴘᴏ 💞", url=f"https://github.com/LEGENDARY-OS/AIMusicX")
+                ]
+            ]
+        ),
+    )
